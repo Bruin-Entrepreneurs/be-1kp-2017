5795e87d96c947c09040eaba5fa7c354d0759d06
Hao Nguyen <haolnguyen7@gmail.com>
2017-08-07 01:43:47 -0700

    clean up

--- be-1kp-2017/README.md
+++
@@ -1,7 +0,0 @@
-# BE One Thousand Pitches
-
-# Backend - Node.js
-### To start app, run either node 1kp.js or npm start
-
-# Frontend - Vanilla for now
-
--- be-1kp-2017/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
-  MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID,
-  MONGOURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/bruin_entrepreneurs'
-}
